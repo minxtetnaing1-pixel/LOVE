e99818363828f65412e3444c263c9d11447b41f2
minxtetnaing1-pixel <minxtetnaing1@gmail.com>
2026-09-13 11:54:15 +0900

    Add files via upload

---
+++ love file.py
@@ -0,0 +1,24 @@
+import time
+
+name = input("Enter your name: ")
+
+print("Hello, " + name + "! ❤")
+time.sleep(1)
+
+print("I have something to tell you...☪☯✡⏩")
+time.sleep(2)
+
+print("3")
+time.sleep(1)
+
+print("2")
+time.sleep(1)
+
+print("1")
+time.sleep(1)
+
+print()
+print("☪✌✋✨ SUPRISE!!! ✨✨✨")
+print("❤️I LOVE YOU SO MUCH, " + name + "!❤")
+print("Keep your smiling and be happy! ❤")
+print("❤❤❤")
