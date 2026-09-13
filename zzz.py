f4370a3922d52206b85553123abe5cdb80cf01e3
minxtetnaing1-pixel <minxtetnaing1@gmail.com>
2026-09-13 11:55:43 +0900

    Add files via upload

---
+++ zzz.py
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>For You ❤️</title>
+</head>
+
+<body>
+
+<h1>Hey ❤️</h1>
+
+<p>I have something to tell you... 🤫</p>
+
+<h2>3️⃣</h2>
+<h2>2️⃣</h2>
+<h2>1️⃣</h2>
+
+<h1>🎉 SURPRISE!!! 🎉</h1>
+
+<h2>❤️ I Love You So Much ❤️</h2>
+
+<p>🥰🌹💖✨</p>
+
+</body>
+</html>
